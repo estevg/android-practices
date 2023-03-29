8e7d58288b1cfcd8c8e0bad5a47bdabc3ba918c8
Esteban Vega <iestebanvega1@gmail.com>
2023-03-29 13:25:14 -0500

    AlertDialog

---
+++ app/src/main/java/com/example/estudioandroid/alertDialog/AlertDialogActivity.kt
@@ -0,0 +1,11 @@
+package com.example.estudioandroid.alertDialog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AlertDialogActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alert_dialog)
+    }
+}
